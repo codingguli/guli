478458ae6982c4fa032770657e832e8db31891ae
故里 <10065162+positionx@user.noreply.gitee.com>
2024-12-18 16:34:48 +0800

    登陆页

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import getWelcome from "@/utils/welcome";
 import style from "./login.module.scss";
 
 const Login: React.FC = () => {
-  
+  const [welcome, setWelcome] = useState({
+    condolence: "",
+    icon: "",
+    welcome: "",
+  });
+
+  useEffect(() => {
+    const hour = dayjs().hour();
+    setWelcome(getWelcome(hour));
+  }, []);
+
+  console.log("welcome", welcome);
+
   return (
     <div className={style.login}>
-        <div className={style['login-left']}>
-          <div className={style['login-left-content']}>
-            <i></i>
-            <p>傍晚了</p>
-            <p>生活从来都不简单，但向前走就对了</p>
-          </div>
+      <div className={style["login-left"]}>
+        <div className={style["login-left-content"]}>
+          <i className={style[`${welcome.icon}`]}></i>
+          <p>{welcome.condolence}</p>
+          <p>{welcome.welcome}</p>
         </div>
-        <div className={style['login-right']}></div>
+      </div>
+      <div className={style["login-right"]}>
+        <div className={style['login-right-container']}>
+          <p>还没账号? <i>去注册</i></p>
+          <div></div>
+          <p>Copyright ©2013-2024 八度云计算（安徽）有限公司版权所有</p>
+        </div>
+      </div>
     </div>
   );
 };
